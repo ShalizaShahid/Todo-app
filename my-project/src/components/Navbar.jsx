2e01e80271202c4a9a9b356e0135f4d69b690bc3
Shaliza Shahid <shaliza847@gmail.com>
2026-07-18 22:40:38 +0500

    Update Navbar.jsx

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -6,16 +6,9 @@ function Navbar(props) {
       <nav className="flex justify-between bg-violet-950 w-full h-16 text-white py-4">
 
         <div className="logo">
-          <span className="font-bold italic mx-8 cursor-pointer text-xl">iTask
+          <span className="font-bold italic mx-8 cursor-pointer text-3xl">iTask
           </span>
         </div>
-
-        <ul className="flex gap-8 mx-18 cursor-pointer transition-all duration-1000">
-
-          <li>{props.home}</li>
-          <li>{props.tasks}</li>
-
-        </ul>
 
       </nav>
 
