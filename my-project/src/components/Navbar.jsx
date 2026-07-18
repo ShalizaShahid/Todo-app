236bdb8bcb38848856c12e666f606aa176c5c1e5
Shaliza Shahid <shaliza847@gmail.com>
2026-07-18 22:58:36 +0500

    Update Navbar.jsx

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(props) {
       <nav className="flex justify-between bg-violet-950 w-full h-16 text-white py-4">
 
         <div className="logo">
-          <span className="font-bold italic mx-8 cursor-pointer text-xl">iTask
+          <span className="font-bold italic mx-8 cursor-pointer text-lg">iTask
           </span>
         </div>
 
